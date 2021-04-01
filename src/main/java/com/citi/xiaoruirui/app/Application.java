280c035238c35e06fa3067c3d33aed110fe71362
Administrator <1445574013@qq.com>
2021-04-01 10:38:57 +0800

    1

--- src/main/java/com/citi/xiaoruirui/app/Application.java
+++ src/main/java/com/citi/xiaoruirui/app/Application.java
@@ -5,6 +5,7 @@ import com.citi.xiaoruirui.service.panel.PanlUnit;
 
 public class Application {
     public static void main(String[] args) throws Exception {
+
         PanlUnit panlUnit = new PanlUnit();
         panlUnit.createPanl();
     }
